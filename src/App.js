62caa354671ef0a3bcc8bc2cf6df8d1f4f66f209
Oh Young-rong <youngrong.oh@gmail.com>
2021-04-18 23:20:53 +0900

    feat: 특정 props 유무에 따라 스타일 변경

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Circle = styled.div`
   width: 5rem;
   height: 5rem;
   background: ${(props) => props.color || "black"};
   border-radius: 50%;
+  /* huge 라는 props 를 설정됐을 때 크기 변경 */
+  ${(props) =>
+    props.huge &&
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
 `;
 
 export default function App() {
-  return <Circle color="blue" />;
+  return <Circle color="red" huge />;
 }
